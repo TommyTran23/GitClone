97db56f86e3f09f565d6aed49765da617b0e0d77
Tommy Tran <ttran5593@protonmail.com>
2020-02-11 12:14:15 -0500

    help function

--- src/idiot.clj
+++ src/idiot.clj
@@ -1,2 +1,15 @@
 (ns idiot)
 
+;; help command
+(defn help [args]
+  (cond
+    (or (= "-h" args) (= "--help" args)) (println "idiot: the other stupid content tracker\n\nUsage: idiot <command> [<args>]\n\nCommands:\n   help\n   init\n   hash-object [-w] <file>\n   cat-file -p <address>")
+    (= "init" args) (println "Usage: idiot init")
+    (= "help" args) (println "Usage: idiot help <command>")
+    (= "hash-object" args) (println "Usage: idiot hash-object [-w] <file>")
+    :else (println "Error: invalid command\n")))
+
+(defn -main [& args]
+  (cond
+    (= "help" (first args)) (help (second args))
+    ))
